f28bab042ab9a6f91843416dc7c0dfe356434132
Maxim <58073342+mhpdd@users.noreply.github.com>
2026-05-01 17:49:58 +0700

    add stats limit + filter runes

--- script.js
+++ script.js
@@ -3,16 +3,19 @@
 // @namespace    http://tampermonkey.net/
 // @version      2026-05-01
 // @description  try to take over the world!
-// @author       mahus
+// @author       You
 // @match        https://questlog.gg/throne-and-liberty/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
 // @grant        GM_log
 // ==/UserScript==
 
 const loggerName = "QuestlogT1"
-const maxGrade = 41;
+const itemsMaxGrade = 41;
+const runesMaxGrade = 41;
 const ignoredGrades = [];
 const stats = ["str", "dex", "int", "per", "con"]
+const maxStats = 49;
+let prevStats = 0;
 
 function log(text) {
     console.log(`${loggerName} > ${text}`);
@@ -34,14 +37,21 @@ async function modifyResponse(response, action) {
 
 function modifyEquipmentItems(json) {
     json.result.data = Object.fromEntries(
-        Object.entries(json.result.data).filter(([key, value]) => value.grade <= maxGrade && !ignoredGrades.includes(value.grade))
+        Object.entries(json.result.data).filter(([key, value]) => value.grade <= itemsMaxGrade && !ignoredGrades.includes(value.grade))
+    );
+    return json
+}
+
+function modifyEquipmentRunes(json) {
+        json.result.data = Object.fromEntries(
+        Object.entries(json.result.data).filter(([key, value]) => value.grade <= runesMaxGrade)
     );
     return json
 }
 
 function modifyRuneSynergies(json) {
     json.result.data = Object.fromEntries(
-        Object.entries(json.result.data).filter(([key, value]) => value.grade <= maxGrade && !ignoredGrades.includes(value.grade))
+        Object.entries(json.result.data).filter(([key, value]) => value.grade <= runesMaxGrade)
     );
 
     let data = json.result.data;
@@ -54,6 +64,31 @@ function modifyRuneSynergies(json) {
     }
 
     return json
+}
+
+function freezeStatsButtons() {
+    let statsText = document.querySelector('.flex.h-8.rounded-t.bg-dark.flex-center').textContent;
+    let currentStats = parseInt(statsText.match(/\d+/));
+    log("stats: " + currentStats)
+
+    if (prevStats != currentStats) {
+        const statsContainer = document.querySelector(".flex.flex-col.gap-2.p-2");
+        const statsIncrementButtons = statsContainer.querySelectorAll('button.inline-flex.bg-light.text-default');
+
+        statsIncrementButtons.forEach(btn => {
+            const isPlus = btn.querySelector('.i-ph\\:plus-bold'); 
+            
+            if (isPlus && currentStats >= maxStats) {
+                btn.style.opacity = '0.2';
+                btn.style.cursor = 'pointer';
+                btn.disabled = true;
+            } else {
+                btn.style.opacity = '1';
+                btn.style.cursor = 'pointer';
+                btn.disabled = false;
+            }
+        });
+    }
 }
 
 (function() {
@@ -72,6 +107,9 @@ function modifyRuneSynergies(json) {
                 case response.url.includes("characterBuilder.getEquipmentItems"):
                     return modifyResponse(response, (json) => modifyEquipmentItems(json));
 
+                case response.url.includes("characterBuilder.getEquipmentRunes"):
+                    return modifyResponse(response, (json) => modifyEquipmentRunes(json));
+
                 // fix rune synregy stats
                 case response.url.includes("characterBuilder.getRuneSynergies"):
                     return modifyResponse(response, (json) => modifyRuneSynergies(json));
@@ -80,4 +118,7 @@ function modifyRuneSynergies(json) {
 
         return response;
     }
+
+    const observer = new MutationObserver(freezeStatsButtons);
+    observer.observe(document.body, { childList: true, subtree: true, characterData: true });
 })();
